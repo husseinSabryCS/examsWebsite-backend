c2f440b5ab111f515fc7f087f34bcaf7f23b075e
husseinSabryCS <sabryhusseinhussein@gmail.com>
2024-06-27 00:26:03 +0300

    add api deleteExam Route

--- scr/routes/adminRoutes.js
+++ scr/routes/adminRoutes.js
@@ -6,4 +6,5 @@ const adminController = require('../controllers/adminController');
 router.post('/add-exam', Token.verifyToken,adminController.addExam);
 router.get('/getSudentsGrad',Token.verifyToken, adminController.getStudentCorrectAnswersCount);
 router.get('/getExamsCreatedByUser/:userId',Token.verifyToken, adminController.getExamsCreatedByUser);
+router.delete('/exams/:examId', Token.verifyToken, adminController.deleteExam);
 module.exports = router;
