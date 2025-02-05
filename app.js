2e13a1a2bfa41aa1115efd51e0bc89b5de57d01b
husseinSabryCS <sabryhusseinhussein@gmail.com>
2025-02-05 10:35:00 +0200

    allow cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(cors({
-  origin: 'https://webexams.vercel.app', 
+  origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
